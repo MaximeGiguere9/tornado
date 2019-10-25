992ac2324727c2efff10a318d6f70e35b8fada8b
Maxime Giguere <maximeetiennegiguere@gmail.com>
2019-10-24 20:49:20 -0400

    Add tornado control scripts

--- Assets/Scripts/Actors/PlayerAvatar.cs
+++ Assets/Scripts/Actors/PlayerAvatar.cs
@@ -1,10 +1,24 @@
-﻿namespace Actors
+﻿using UnityEngine;
+
+namespace Actors
 {
 	/// <summary>
 	/// Player binding that possesses a tornado
 	/// </summary>
-	public class PlayerAvatar
+	public class PlayerAvatar : MonoBehaviour
 	{
-		
+		[SerializeField] private Tornado tornado;
+		[SerializeField] private Color playerColor;
+
+		private void Start()
+		{
+			this.tornado.SetCursorColor(this.playerColor);
+		}
+
+		private void Update()
+		{
+			this.tornado.SetRageActive(Input.GetButton("Fire1"));
+			this.tornado.SetMoveDirection(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
+		}
 	}
 }
--- Assets/Scripts/Actors/Tornado.cs
+++ Assets/Scripts/Actors/Tornado.cs
@@ -7,16 +7,47 @@ namespace Actors
 	/// </summary>
 	public class Tornado : MonoBehaviour
 	{
-		// Start is called before the first frame update
-		void Start()
-		{
+		[SerializeField] private ParticleSystem rageParticles;
+		[SerializeField] private ParticleSystem cursorParticles;
+		[SerializeField] private new Rigidbody rigidbody;
+		[SerializeField] private float turnRate;
+		[SerializeField] private float maxSpeed;
+		private Vector3 moveDirection;
+		private bool rageActive = true;
 
+		private void FixedUpdate()
+		{
+			this.rigidbody.AddForce(this.moveDirection * this.turnRate);
+			this.rigidbody.velocity = Vector3.ClampMagnitude(this.rigidbody.velocity, this.maxSpeed);
 		}
 
-		// Update is called once per frame
-		void Update()
+		public void SetRageActive(bool active)
 		{
+			if (this.rageActive == active) return;
+			this.rageActive = active;
 
+			if (active)
+			{
+				this.rageParticles.Play();
+			}
+			else
+			{
+				this.rageParticles.Stop();
+				this.rageParticles.Clear();
+			}
+				
+		}
+
+		public void SetCursorColor(Color color)
+		{
+			ParticleSystem.MainModule m = this.cursorParticles.main;
+			m.startColor = color;
+		}
+
+		public void SetMoveDirection(Vector2 direction)
+		{
+			this.moveDirection = new Vector3(direction.x, 0, direction.y);
+			this.moveDirection = this.moveDirection.normalized;
 		}
 	}
 }
