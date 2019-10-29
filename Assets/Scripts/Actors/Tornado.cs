d2be59f079fd99d514fd19bf4cc2ae128bd6c0c6
Maxime Giguere <maximeetiennegiguere@gmail.com>
2019-10-29 12:10:20 -0400

    Merge TornadoGrabber into main Tornado script
    No reason to use their functionalities separately

--- Assets/Scripts/Actors/Tornado.cs
+++ Assets/Scripts/Actors/Tornado.cs
@@ -1,9 +1,11 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 namespace Actors
 {
 	/// <summary>
-	/// Controls a tornado avatar (movements, rage, strength)
+	/// Controls a tornado avatar (movements, rage, strength, grab)
 	/// </summary>
 	public class Tornado : MonoBehaviour
 	{
@@ -13,21 +15,63 @@ namespace Actors
 		[SerializeField] private Collider rangeTrigger;
 		[SerializeField] private float turnRate;
 		[SerializeField] private float maxSpeed;
+		[SerializeField] private PlayerAvatar player;
+
+		private HashSet<GrabbableObject> orbitingObjects;
 		private Vector3 moveDirection;
-		private bool rageActive = true;
+		private bool isRageActive = true;
+
+		private void Start()
+		{
+			this.orbitingObjects = new HashSet<GrabbableObject>();
+		}
 
 		private void FixedUpdate()
 		{
+			//move tornado
 			this.rigidbody.AddForce(this.moveDirection * this.turnRate);
 			this.rigidbody.velocity = Vector3.ClampMagnitude(this.rigidbody.velocity, this.maxSpeed);
+
+			//define orbit of each object randomly based on their hash code
+			foreach (GameObject go in this.orbitingObjects.Select(grabbable => grabbable.gameObject))
+			{
+				float height = go.GetHashCode() % 200 + 100;
+				float radius = go.GetHashCode() % 80 + 30;
+				float rotationSpeed = go.GetHashCode() % 5 + 2;
+				float angle = go.GetHashCode() % 2 * Mathf.PI;
+
+				Vector3 localPos = new Vector3(
+					radius * Mathf.Cos(Time.fixedTime * rotationSpeed + angle),
+					height,
+					radius * Mathf.Sin(Time.fixedTime * rotationSpeed + angle)
+				);
+
+				go.transform.position = Vector3.Lerp(
+					go.transform.position,
+					gameObject.transform.position + localPos,
+					0.15f
+				);
+			}
 		}
 
-		public bool IsRageActive() => this.rageActive;
+		private void OnTriggerEnter(Collider other)
+		{
+			if (!this.isRageActive) return;
+
+			//add object to tornado
+			GrabbableObject grabbable = other.GetComponent<GrabbableObject>();
+			if (grabbable == null) return;
+
+			this.orbitingObjects.Add(grabbable);
+			grabbable.Grab(this.player.GetPlayerID());
+		}
+
+		public bool IsRageActive() => this.isRageActive;
 
 		public void SetRageActive(bool active)
 		{
-			if (this.rageActive == active) return;
-			this.rageActive = active;
+			if (this.isRageActive == active) return;
+			this.isRageActive = active;
 
 			if (active)
 			{
@@ -37,6 +81,7 @@ namespace Actors
 			{
 				this.rageParticles.Stop();
 				this.rageParticles.Clear();
+				ReleaseOrbitingObjects();
 			}
 
 			this.rangeTrigger.enabled = active;
@@ -52,6 +97,13 @@ namespace Actors
 		{
 			this.moveDirection = new Vector3(direction.x, 0, direction.y);
 			this.moveDirection = this.moveDirection.normalized;
+		}
+
+		private void ReleaseOrbitingObjects()
+		{
+			foreach (GrabbableObject grabbable in this.orbitingObjects)
+				grabbable.Release();
+			this.orbitingObjects.Clear();
 		}
 	}
 }
